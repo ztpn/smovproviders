f1b02800f73b0cdfe509d50a5dbedae719441be5
TPN <tpn@ainta.dev>
2024-07-03 14:57:54 +0100

    Disable Nepu

--- src/providers/sources/nepu/index.ts
+++ src/providers/sources/nepu/index.ts
@@ -9,6 +9,7 @@ import { NotFoundError } from '@/utils/errors';
 import { SearchResults } from './types';
 
 const nepuBase = 'https://nepu.io';
+const nepuReferer = 'https://nepu.to';
 
 const universalScraper = async (ctx: MovieScrapeContext | ShowScrapeContext) => {
   const searchResultRequest = await ctx.proxiedFetcher<string>('/ajax/posts', {
@@ -63,7 +64,11 @@ const universalScraper = async (ctx: MovieScrapeContext | ShowScrapeContext) => 
         captions: [],
         playlist: streamUrl[1],
         type: 'hls',
-        flags: [flags.CORS_ALLOWED],
+        headers: {
+          Origin: nepuReferer,
+          Referer: `${nepuReferer}/`,
+        },
+        flags: [],
       },
     ],
   } as SourcererOutput;
@@ -73,7 +78,8 @@ export const nepuScraper = makeSourcerer({
   id: 'nepu',
   name: 'Nepu',
   rank: 80,
-  flags: [flags.CORS_ALLOWED],
+  disabled: true,
+  flags: [],
   scrapeMovie: universalScraper,
   scrapeShow: universalScraper,
 });
